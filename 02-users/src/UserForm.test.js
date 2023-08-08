2ea8d809abbc474a5e6105c291ae8637c610db7d
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2023-08-08 15:10:06 +1000

    completion of UserForm.test.js, ensuring async/await added properly

--- 02-users/src/UserForm.test.js
+++ 02-users/src/UserForm.test.js
@@ -17,12 +17,17 @@ test('it shows two inputs and a button', () => {
 })
 
 test('it calls onUserAdd when the form is submitted', async () => {
-	// NOT THE BEST IMPLEMENTATION
+	const mock = jest.fn()
 	// Try to render my component
-	render(<UserForm />)
+	render(<UserForm onUserAdd={mock} />)
 
 	// Find the two inputs
-	const [nameInput, emailInput] = screen.getAllByRole('textbox')
+	const nameInput = screen.getByRole('textbox', {
+		name: /name/i,
+	})
+	const emailInput = screen.getByRole('textbox', {
+		name: /email/i,
+	})
 
 	// Simulate typing in a name
 	await user.click(nameInput)
@@ -33,8 +38,12 @@ test('it calls onUserAdd when the form is submitted', async () => {
 	await user.keyboard('jane@jane.com')
 
 	// Find the button
+	const button = screen.getByRole('button')
 
 	// Simulate clicking the button
+	await user.click(button)
 
 	// Assertion to make sure 'onUserAdd' gets called with email/name
+	expect(mock).toHaveBeenCalled()
+	expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@jane.com' })
 })
